c601ba84e06e964d06811c7182bea3b9c701d47e
sunny jain <xesunny@gmail.com>
2020-04-22 11:20:47 +0800

    First commit.

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+
+exports.ETH_SCAN_API = "http://api.etherscan.io/api?module=account&action=txlist&apikey=" + process.env.API_KEY + "&sort=asc&page=1&offset=10&";
+
+                        // + "address="+ "0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a" 
+                        // + "&startblock=0&"
+                        // + "endblock=99999999";
+
